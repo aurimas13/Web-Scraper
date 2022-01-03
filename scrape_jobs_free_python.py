7db0f194eebba38eea0281e792f579becca93849
Aurimas A. Nausedas <aurimas.nausedas@gmail.com>
2022-01-03 16:46:07 +0200

    upodated python file

--- scrape_jobs_free_python.py
+++ scrape_jobs_free_python.py
@@ -3,23 +3,17 @@ from bs4 import BeautifulSoup
 
 # URL where to search
 URL = "https://pythonjobs.github.io/"
-# URL = "https://realpython.github.io/fake-jobs/"
+# Getting a website
 page = requests.get(URL)
 html_doc = "python"
 
 # Implementing BeautifySoup
 soup = BeautifulSoup(page.content, "html.parser")
-bs = BeautifulSoup(html_doc, features="html.parser")
-# Taking the results found in 'job_list' class
-results = soup.find(class_="job_list")
 
-job_elements = results.find_all("div", class_="job")
-
-
-print([item["data-tags"] for item in bs.find_all() if "data-tags" in item.attrs])
-print(results)
-
-
+# Printing the available job positions
+for row in soup.find_all('h1', a=False, href=False, class_=False):
+    if row.text != 'Most Recent Jobs':
+        print(row.text)
 
 
 
